6cf440f9275df013abdc1db814b3ae437ed09d04
Ivan Marton <ivan.marton@onekey.com>
2024-09-06 14:34:51 +0200

    refact(lang): Make the terraform lang condfig nix-lint conform.

--- config/lang/terraform.nix
+++ config/lang/terraform.nix
@@ -1,9 +1,15 @@
 {
   plugins = {
     lsp.servers.terraformls.enable = true;
-    none-ls.sources.formatting.terraform_fmt.enable = true;
-    none-ls.sources.diagnostics.terraform_validate.enable = true;
-    none-ls.sources.formatting.terragrunt_fmt.enable = true;
-    none-ls.sources.diagnostics.terragrunt_validate.enable = true;
+    none-ls.sources = {
+      formatting ={
+        terraform_fmt.enable = true;
+        terragrunt_fmt.enable = true;
+      };
+      diagnostics = {
+        terraform_validate.enable = true;
+        terragrunt_validate.enable = true;
+      };
+    };
   };
 }
