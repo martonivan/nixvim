511cb13a0effb58d614130c0467605a7a10e29f1
Ivan Marton <ivan.marton@onekey.com>
2024-09-12 12:44:14 +0200

    Imporve python dev support

--- config/lang/python.nix
+++ config/lang/python.nix
@@ -2,36 +2,10 @@
   plugins = {
     lsp = {
       servers = {
+        pyright.enable = true;
         ruff.enable = true;
         ruff-lsp.enable = true;
       };
     };
-    none-ls = {
-      sources = {
-        formatting.black.enable = true;
-        diagnostics = { mypy.enable = true; };
-      };
-    };
   };
-
-  extraPackages = with pkgs; [ 
-    sourcery
-  ];
-  # TODO: sops for token
-  extraConfigLua = # lua
-    ''
-      -- local lspconfig = require('lspconfig')
-      -- lspconfig.sourcery.setup {
-      --   -- Server-specific settings. See `:help lspconfig-setup`
-      --   settings = {
-      --     ['sourcery'] = {
-      --       init_options = {
-      --         token = "<to be placed by sops.nix>",
-      --         extension_version = "vim.lsp",
-      --         editor_version = "vim",
-      --       },
-      --     },
-      --   },
-      -- }
-    '';
 }
